540049b8ad549ad44455ae2f5811711995e03da1
Thomas Rometsch <thomas.rometsch@gmail.com>
2021-02-18 16:02:09 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="vortector",
+    version="1.0",
+    description="A vortex detector for planet-disk simulation data.",
+    author="Thomas Rometsch",
+    author_email="thomas.rometsch@uni-tuebingen.de",
+    url="https://github.com/rometsch/vortector",
+    py_modules=["vortector"],
+    install_requires=[
+        "numpy", "matplotlib", "opencv-python"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Utilities"
+    ],
+    python_requires='>=3.6'
+)
